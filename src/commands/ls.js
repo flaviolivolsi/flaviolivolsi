e61c6a7a2a51f340ae75c9bd400b256f3441bfba
Flavio Li Volsi <flavio.livolsi@gmail.com>
2019-06-30 13:05:26 +0200

    Adding  to target _blank link

--- src/commands/ls.js
+++ src/commands/ls.js
@@ -1,46 +1,39 @@
-import React from 'react';
-import path from 'path';
-import utils from '../utils';
-import filesystem from '../data/filesystem';
+import React from "react";
+import path from "path";
+import utils from "../utils";
+import filesystem from "../data/filesystem";
 
 /**
  * @TODO: show error when args leads to not existing dir
  */
 
-const List = ({ args = '.', state }) => {
+const List = ({ args = ".", state }) => {
   const formatDir = () => {
     const directory = path.resolve(state.currentDirectory, args);
 
-    const children = directory === '/' ?
-      filesystem : 
-      utils.listDir(directory).children;
+    const children =
+      directory === "/" ? filesystem : utils.listDir(directory).children;
 
     if (!children) {
-      return <span></span>;
+      return <span />;
     }
 
     return children.map((value, key) => {
-      if (value.action && value.action === 'link') {
+      if (value.action && value.action === "link") {
         return (
           <span key={key}>
-            <a href={value.target} target="_blank">{value.name}</a>{" "}
+            <a href={value.target} target="_blank" rel="noopener noreferrer">
+              {value.name}
+            </a>{" "}
           </span>
         );
       }
 
-      return (
-        <span key={key}>
-          {value.name}{" "}
-        </span>
-      );
+      return <span key={key}>{value.name} </span>;
     });
   };
 
-  return (
-    <span className="ls">
-      {formatDir()}
-    </span>
-  );
+  return <span className="ls">{formatDir()}</span>;
 };
 
 export default List;
